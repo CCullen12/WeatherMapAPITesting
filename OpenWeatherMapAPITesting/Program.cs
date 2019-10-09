a6f9b7316d878fb61d02c9ac6a735e1410fc47a3
Ciaran Cullen <CCullen@spartaglobal.com>
2019-10-09 09:33:13 +0100

    Secound Commit, Framework for api calling and saving should be done, now to fill in the details needed.

--- OpenWeatherMapAPITesting/Program.cs
+++ OpenWeatherMapAPITesting/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//this is a console application just in case i need to print out
+//many variables.
 namespace OpenWeatherMapAPITesting
 {
     class Program
